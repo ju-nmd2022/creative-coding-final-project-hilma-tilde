7262f2e6551ff20d2a83abbcfaebecd7edeb5678
tildehannevad <hati22fq@student.ju.se>
2024-10-01 11:43:21 +0200

    mood checker

--- sketch.js
+++ sketch.js
@@ -12,26 +12,26 @@ function setup() {
   video = createCapture(VIDEO);
   video.size(640, 480);
   video.hide();
-  
+
   // Load the hand tracking model
-  handTrack.load().then(lmodel => {
+  handTrack.load(handTrackConfig).then((lmodel) => {
     model = lmodel;
   });
-  
+
   friend = new LittleFriend();
 }
 
 function draw() {
   background(200);
-  
+
   image(video, 0, 0);
 
   if (model) {
-    model.detect(video.elt).then(preds => {
+    model.detect(video.elt).then((preds) => {
       predictions = preds;
     });
   }
-  
+
   friend.update(predictions);
   friend.display();
 }
@@ -42,26 +42,34 @@ class LittleFriend {
     this.y = height / 2;
     this.energy = 100;
   }
-  
+
   update(predictions) {
     if (predictions.length > 0) {
       let hand = predictions[0].bbox;
       this.x = hand[0] + hand[2] / 2;
       this.y = hand[1] + hand[3] / 2;
-      
+
       // Random responses based on hand positions
       if (hand[2] > 100) {
         this.energy = random(50, 150);
       }
     }
   }
-  
+
   display() {
     fill(255, 0, 0);
     ellipse(this.x, this.y, 50, 50);
     fill(0);
     textSize(16);
     textAlign(CENTER, CENTER);
-    text('Energy: ' + this.energy.toFixed(2), this.x, this.y + 30);
+    text("Energy: " + this.energy.toFixed(2), this.x, this.y + 30);
   }
 }
+
+// Configuration for handTrack
+const handTrackConfig = {
+  flipHorizontal: true, // flip e.g for video
+  maxNumBoxes: 1, // maximum number of boxes to detect
+  iouThreshold: 0.5, // ioU threshold for non-max suppression
+  scoreThreshold: 0.6, // confidence threshold for predictions.
+};
